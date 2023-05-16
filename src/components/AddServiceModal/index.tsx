96e5ff41f34ab33c170fd995b2819d69c5e3d5a2
XXQ <64342877+XXQ227@users.noreply.github.com>
2023-05-16 03:56:12 -0400

    Update index.tsx

--- src/components/AddServiceModal/index.tsx
+++ src/components/AddServiceModal/index.tsx
@@ -38,57 +38,6 @@ const AddServiceModal: React.FC<Props> = (props) => {
     const [transportID, setTransportID] = useState(0);
     const [checkedValue, setCheckedValue] = useState('');
 
-    /*useLayoutEffect(() => {
-        const labels = document.querySelectorAll('.ant-radio-button-wrapper label');
-        const icons = document.querySelectorAll('.ant-radio-button-wrapper svg');
-        const wrappers = document.querySelectorAll('.ant-radio-button-wrapper');
-
-        console.log(labels)
-        console.log(icons)
-        console.log(wrappers)
-
-        const updateWrapperStyle = (index: number) => {
-            wrappers.forEach((wrapper, wrapperIndex) => {
-                console.log(index)
-                console.log(wrapperIndex)
-                if (index === wrapperIndex) {
-                    wrapper.classList.add('ant-radio-button-wrapper-checked');
-                } else {
-                    wrapper.classList.remove('ant-radio-button-wrapper-checked');
-                }
-            });
-        };
-
-        labels.forEach((label, index) => {
-            label.addEventListener('click', () => {
-                console.log(111111111)
-                updateWrapperStyle(index);
-            });
-        });
-
-        icons.forEach((icon, index) => {
-            icon.addEventListener('click', () => {
-                updateWrapperStyle(index);
-            });
-        });
-
-        // 在组件卸载时清除事件监听器
-        return () => {
-            labels.forEach((label, index) => {
-                label.removeEventListener('click', () => {
-                    console.log(45645135156)
-                    updateWrapperStyle(index);
-                });
-            });
-
-            icons.forEach((icon, index) => {
-                icon.removeEventListener('click', () => {
-                    updateWrapperStyle(index);
-                });
-            });
-        };
-    }, []); // 空数组作为第二个参数确保仅在组件挂载时执行一次*/
-
     const serviceList = [
         {
             serviceName: 'Sea Import',
@@ -154,48 +103,6 @@ const AddServiceModal: React.FC<Props> = (props) => {
         { label: 'Air Export', value: 'Air Export', icon: 'icon-air' },
     ];
 
-    /*const options = [
-        { label: 'Sea Import', value: 'Sea Import', icon: <IconFont type={'icon-sea'}/> },
-        { label: 'Sea Export', value: 'Sea Export', icon: <IconFont type={'icon-sea'}/> },
-        { label: 'Cross-border', value: 'Cross-border', icon: <IconFont type={'icon-truck'} /> },
-        { label: 'Air Import', value: 'Air Import', icon: <IconFont type={'icon-air'} /> },
-        { label: 'Air Export', value: 'Air Export', icon: <IconFont type={'icon-air'} /> },
-    ];*/
-
-    /*const options = [
-        { label: <label onClick={(e) => handleRadioChange(e.target.innerText)}>Sea Import</label>, value: 'Sea Import', icon: <IconFont type={'icon-sea'}/> },
-        { label: <label>Sea Export</label>, value: 'Sea Export', icon: <IconFont type={'icon-sea'}/> },
-        { label: <label>Cross-border</label>, value: 'Cross-border', icon: <IconFont type={'icon-truck'} /> },
-        { label: <label>Air Import</label>, value: 'Air Import', icon: <IconFont type={'icon-air'} /> },
-        { label: <label>Air Export</label>, value: 'Air Export', icon: <IconFont type={'icon-air'} /> },
-    ];*/
-
-    /*const labels = document.querySelectorAll('.ant-radio-button-wrapper label');
-    const icons = document.querySelectorAll('.ant-radio-button-wrapper svg');
-    const wrappers = document.querySelectorAll('.ant-radio-button-wrapper');
-
-    labels.forEach((label, index) => {
-        label.addEventListener('click', () => {
-            updateWrapperStyle(index);
-        });
-    });
-
-    icons.forEach((icon, index) => {
-        icon.addEventListener('click', () => {
-            updateWrapperStyle(index);
-        });
-    });
-
-    function updateWrapperStyle(index: number) {
-        wrappers.forEach((wrapper, wrapperIndex) => {
-            if (index === wrapperIndex) {
-                wrapper.classList.add('ant-radio-button-wrapper-checked');
-            } else {
-                wrapper.classList.remove('ant-radio-button-wrapper-checked');
-            }
-        });
-    }*/
-
     return (
         <Modal
             className={styles.addServiceModal}
@@ -216,12 +123,6 @@ const AddServiceModal: React.FC<Props> = (props) => {
             ]}
         >
             <Divider orientation="left">Service Type</Divider>
-            {/*<Radio.Group className={styles.antRadio}>
-                {options.map(option => (
-                    <CustomRadio key={option.value} option={option} />
-                ))}
-            </Radio.Group>*/}
-
             <Radio.Group className={styles.antRadio} value={checkedValue} onChange={(e) => handleRadioChange(e.target.value)}>
                 {options.map(option => (
                     <CustomRadio key={option.value} option={option} checked={checkedValue === option.value} onChange={handleRadioChange} />
